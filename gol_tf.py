f8a418c1e866313b5efd382225d642d73dd4e706
dylan <dylandrover@gmail.com>
2017-01-31 01:46:12 -0500

    Need to not reinit env every loop

--- gol_tf.py
+++ gol_tf.py
@@ -8,16 +8,14 @@ dim1 = int(sys.argv[1])
 dim2 = int(sys.argv[2])
 gen = int(sys.argv[3])
 
-
-
 # Declare tf variables
 #env  = tf.Variable(tf.abs(tf.round(tf.truncated_normal(shape=(dim1, dim2),
                                                        #mean=0, stddev=1))), name='state')
 
-env = tf.Variable([[0, 0, 0, 0, 0],[0, 0, 1, 0, 0],[0, 0, 1, 0, 0],[0, 0, 1, 0, 0],[0, 0, 0, 0, 0]], dtype=tf.float32)
+seed = tf.Variable([[0, 0, 0, 0, 0],[0, 0, 1, 0, 0],[0, 0, 1, 0, 0],[0, 0, 1, 0, 0],[0, 0, 0, 0, 0]], dtype=tf.float32)
 neighbour_sum = tf.placeholder(tf.float32, shape=env.get_shape())
 
-
+env = 
 alive_dead    = tf.placeholder(tf.float32, shape=env.get_shape())
 combin = tf.placeholder(tf.float32, shape=env.get_shape())
 and_   = tf.placeholder(tf.float32, shape=env.get_shape())
@@ -40,7 +38,8 @@ combin = tf.multiply(neighbour_sum, alive_dead)
 less = tf.cast(tf.greater(combin, tf.scalar_mul(6, tf.ones(shape=combin.get_shape()))), tf.int32)
 greater = tf.cast(tf.greater_equal(combin, tf.scalar_mul(3, tf.ones(shape=combin.get_shape()))), tf.int32)
 and_ = tf.cast(tf.add(greater,less, name='new_state'), tf.int32)
-
+env = tf.reshape(env, [dim1, dim2])
+env = and_
 '''
 life_summary = tf.summary.image('grid',and_)
 merged = tf.summary.merge_all(life_summary)
@@ -48,7 +47,7 @@ merged = tf.summary.merge_all(life_summary)
 
 init = tf.global_variables_initializer()
 
-sess = tf.Session()
+sess = tf.InteractiveSession()
 
 summary_writer = tf.summary.FileWriter('./tmp/logs/')
 
@@ -56,8 +55,6 @@ for i in range(gen):
     sess.run(init)
     #summary_writer.add_summary(merged, i)
     #env = and_
-#    print(combin.eval(session=sess))
-    #print(greater.eval(session=sess))
     print(env.eval(session=sess))
     print(and_.eval(session=sess))
     time.sleep(0.5)
