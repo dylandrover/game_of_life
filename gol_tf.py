af12ea415da63b4c1731a78b0f1c56ca89270b26
dylan <dylandrover@gmail.com>
2017-02-05 22:18:34 -0500

    Saves Gif and tensorboard file (ex. in folders)

--- gol_tf.py
+++ gol_tf.py
@@ -17,8 +17,9 @@ if(len(sys.argv) == 5):
 
 # Declare tf variables
 env  = tf.Variable(tf.abs(tf.round(tf.truncated_normal(shape=(dim1, dim2),
-                                                       mean=0, stddev=0.5))), name='state')
+                                                       mean=0, stddev=0.375))), name='state')
 '''
+Testing matrix for blinkers/stationary lifeforms
 env = tf.Variable([[0, 0, 0, 0, 0, 0],
                    [0, 0, 1, 1, 0, 0],
                    [0, 1, 0, 0, 1, 0],
@@ -62,24 +63,27 @@ update = env.assign(new_val)
 life_summary = tf.summary.image("board", env_image)
 live_summary = tf.summary.scalar("sum", tf.reduce_sum(new_val))
 
+# Merge summaries to write out to tensorboard
 merged = tf.summary.merge_all()
-summary_writer = tf.summary.FileWriter('./tmp/logs/')
+summary_writer = tf.summary.FileWriter('./tb_summaries/')
 
-if(flag):
+if(flag == 'mpl'):
     ims = []
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(gen):
-        _, summary = sess.run([update, merged])
+        img, summary = sess.run([update, merged])
         summary_writer.add_summary(summary, i)
         if(flag):
             img = plt.imshow(img, cmap='gray')
             ims.append([img])
 
-if(flag):
+# Creates animation of epochs and saves to gif file using imagemagick
+if(flag == 'mpl'):
     ani = animation.ArtistAnimation(fig, ims, interval=100, blit=True, repeat_delay=50)
-    ani.save('life.mp4')
+    ani.save('./images/'+ time.strftime("%H:%M:%S") + '_life.gif', writer='imagemagick', fps=90)
+    plt.show()
 
     
 
